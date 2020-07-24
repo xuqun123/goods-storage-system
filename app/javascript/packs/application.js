b3a6574ee0a5a8fad34785512fb5ee8030671451
dev_Q <xuqunlele@gmail.com>
2020-07-25 00:23:43 +1000

    [Vue Config] 1. move app.vue component to a components sub folder. 2. initialize Vue with app component in application.js. 3. add webpacker 'dev_server_host' config in develoment config file. 4. remove unused 'hello world' vue file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,16 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import Vue from 'vue'
+import App from '../components/app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(document.createElement('app'))
+  const app = new Vue({
+    el: 'app',
+    template: '<App/>',
+    components: { App }
+  })
+
+  console.log(app)
+})
